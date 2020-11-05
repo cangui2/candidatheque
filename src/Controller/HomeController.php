a31fce39f81c4228ac9d6e22e5a4a33b5c6605e1
Dan <in.kd@outlook.fr>
2020-11-05 16:29:10 +0100

    test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,7 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        //test
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
