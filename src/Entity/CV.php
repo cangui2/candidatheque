e66f0524842f4148058da96d59c724096429513a
francois <fcaumartin@gmail.com>
2020-11-13 10:31:44 +0100

    CV Entity

--- src/Entity/CV.php
+++ src/Entity/CV.php
@@ -59,10 +59,39 @@ class CV
      */
     private $consultes;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Experience::class, mappedBy="cv")
+     */
+    private $experiences;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $titre;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Profil::class, mappedBy="cv")
+     */
+    private $profils;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Formation::class, mappedBy="cv")
+     */
+    private $formations;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Langue::class, mappedBy="cv")
+     */
+    private $langues;
+
     public function __construct()
     {
         $this->favoris = new ArrayCollection();
         $this->consultes = new ArrayCollection();
+        $this->experiences = new ArrayCollection();
+        $this->profils = new ArrayCollection();
+        $this->formations = new ArrayCollection();
+        $this->langues = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -193,6 +222,138 @@ class CV
             // set the owning side to null (unless already changed)
             if ($consulte->getCv() === $this) {
                 $consulte->setCv(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Experience[]
+     */
+    public function getExperiences(): Collection
+    {
+        return $this->experiences;
+    }
+
+    public function addExperience(Experience $experience): self
+    {
+        if (!$this->experiences->contains($experience)) {
+            $this->experiences[] = $experience;
+            $experience->setCv($this);
+        }
+
+        return $this;
+    }
+
+    public function removeExperience(Experience $experience): self
+    {
+        if ($this->experiences->removeElement($experience)) {
+            // set the owning side to null (unless already changed)
+            if ($experience->getCv() === $this) {
+                $experience->setCv(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(?string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Profil[]
+     */
+    public function getProfils(): Collection
+    {
+        return $this->profils;
+    }
+
+    public function addProfil(Profil $profil): self
+    {
+        if (!$this->profils->contains($profil)) {
+            $this->profils[] = $profil;
+            $profil->setCv($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProfil(Profil $profil): self
+    {
+        if ($this->profils->removeElement($profil)) {
+            // set the owning side to null (unless already changed)
+            if ($profil->getCv() === $this) {
+                $profil->setCv(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Formation[]
+     */
+    public function getFormations(): Collection
+    {
+        return $this->formations;
+    }
+
+    public function addFormation(Formation $formation): self
+    {
+        if (!$this->formations->contains($formation)) {
+            $this->formations[] = $formation;
+            $formation->setCv($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFormation(Formation $formation): self
+    {
+        if ($this->formations->removeElement($formation)) {
+            // set the owning side to null (unless already changed)
+            if ($formation->getCv() === $this) {
+                $formation->setCv(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Langue[]
+     */
+    public function getLangues(): Collection
+    {
+        return $this->langues;
+    }
+
+    public function addLangue(Langue $langue): self
+    {
+        if (!$this->langues->contains($langue)) {
+            $this->langues[] = $langue;
+            $langue->setCv($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLangue(Langue $langue): self
+    {
+        if ($this->langues->removeElement($langue)) {
+            // set the owning side to null (unless already changed)
+            if ($langue->getCv() === $this) {
+                $langue->setCv(null);
             }
         }
 
