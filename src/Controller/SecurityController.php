7e19714eb060fb98f7afa1df0db48bfdefe8a324
Dan <in.kd@outlook.fr>
2020-11-19 09:44:54 +0100

    travail

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Form\CandidatRegistrationFormType;
+use App\Entity\User;
 
 class SecurityController extends AbstractController
 {
@@ -18,6 +20,9 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        $user = new User();
+        $regForm = $this->createForm(CandidatRegistrationFormType::class, $user);
+
         if($this->isGranted("IS_AUTHENTICATED_REMEMBERED"))
         {
             return $this->redirectToRoute("login_success");
@@ -28,7 +33,7 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error,'regForm' => $regForm->createView(),]);
     }
 
     /**
