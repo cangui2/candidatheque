2f3a4034b4d09d6f4c8051bef850a90b643a1605
francois <fcaumartin@gmail.com>
2020-11-20 18:53:41 +0100

    fix

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -20,8 +20,6 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $user = new User();
-        $regForm = $this->createForm(CandidatRegistrationFormType::class, $user);
 
         if($this->isGranted("IS_AUTHENTICATED_REMEMBERED"))
         {
@@ -33,7 +31,7 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error,'regForm' => $regForm->createView(),]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
