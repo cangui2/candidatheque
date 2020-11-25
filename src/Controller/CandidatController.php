b954b37dd60b1b451d9d8ff22ace53c267a3cfaf
Dislaire Frédéric <dislaire.frederic@hotmail.fr>
2020-11-25 17:44:37 +0100

    Modif du jour

--- src/Controller/CandidatController.php
+++ src/Controller/CandidatController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\PostuleRepository;
 use App\Form\CandidatRegistrationFormType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CandidatController extends AbstractController
 {
@@ -15,8 +16,12 @@ class CandidatController extends AbstractController
     /**
      * @Route("/candidat/dashboard", name="dashboard_candidat")
      */
-    public function dashboard(): Response {
+    public function dashboard(PostuleRepository $repo): Response 
+    {
 
+        $compteur = $repo->countByCandidat(1);
+
+        dd($compteur);
 
         return $this->render('candidat/dashboard_candidat.html.twig');
     }
