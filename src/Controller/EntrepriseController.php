bf9aa4fc1aa99aea6011e902cb91ff33cfa74d09
Julien Canguilieme <canguijc@gmail.com>
2020-11-26 08:44:04 +0100

    mise en place de la requete

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\OffreRepository;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class EntrepriseController extends AbstractController
 {
@@ -26,11 +29,25 @@ class EntrepriseController extends AbstractController
 
     /**
      * @Route("/entreprise/dashboard", name="dashboard_entreprise")
+     * @param SessionInterface $session
+     * @return Response
      */
-    public function dashboard(): Response {
+    public function dashboard(OffreRepository $repo): Response {
 
 
-        return $this->render('entreprise/dashboard_entreprise.html.twig');
+        $recruteur_id = $this->getUser()->getRecruteur()->getId();
+
+
+//        $repository = $this->getDoctrine()
+//            ->getManager()
+//            ->getRepository('OffreRepository:Offre');
+
+        $result = $repo->findCustomOfferByIdRecruteur($recruteur_id);
+
+
+        return $this->render('entreprise/dashboard_entreprise.html.twig',[
+            'session'=>$result,
+        ]);
     }
 
 
