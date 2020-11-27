125a8ac8e85dc52a4dfd73581a2d3a9e210689c7
francois <fcaumartin@gmail.com>
2020-11-27 14:11:40 +0100

    react cv

--- assets/cv/Templates.js
+++ assets/cv/Templates.js
@@ -2,16 +2,16 @@ import React, { useState }  from 'react';
 import { DropdownButton, Dropdown } from 'react-bootstrap';
 
 function Templates() {
-    const [style, setStyle] = useState("/template1.css");
+    const [style, setStyle] = useState("/template3-2.css");
   
     return (
         <span>
         <link rel="stylesheet" href={style} />
           <DropdownButton variant="secondary"  key={1} id="dropdown-basic-button" title="Modèles" >
-              <Dropdown.Item eventKey="/assets/css/template1.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 1</Dropdown.Item>
-              <Dropdown.Item eventKey="/assets/css/template2.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 2</Dropdown.Item>
-              <Dropdown.Item eventKey="/assets/css/template3.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 3</Dropdown.Item>
-              <Dropdown.Item eventKey="/assets/css/template4.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 4</Dropdown.Item>
+              <Dropdown.Item eventKey="/assets/css/template3-2.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 1</Dropdown.Item>
+              <Dropdown.Item eventKey="/assets/css/template3-3.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 2</Dropdown.Item>
+              <Dropdown.Item eventKey="/assets/css/template3-5.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 3</Dropdown.Item>
+              <Dropdown.Item eventKey="/assets/css/template3-7.css" onSelect={ (eventKey) => { setStyle(eventKey); }}>Modèle 4</Dropdown.Item>
           </DropdownButton>
         </span>
     );
