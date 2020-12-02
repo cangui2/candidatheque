bf6d0939ac97a6d405fcaa74ebb33d819ae941bd
Dislaire Frédéric <dislaire.frederic@hotmail.fr>
2020-12-02 17:31:01 +0100

    jeu de test Compétences & requete à revoir

--- src/Controller/CandidatController.php
+++ src/Controller/CandidatController.php
@@ -17,20 +17,27 @@ class CandidatController extends AbstractController
     /**
      * @Route("/candidat/dashboard", name="dashboard_candidat")
      */
-    public function dashboard(PostuleRepository $repo,CandidatRepository $repo2): Response 
+    public function dashboard(PostuleRepository $repo,CandidatRepository $repo2,PostuleRepository $repo3): Response 
     {
 
         $candidat_id = $this->getUser()->getCandidat()->getId();
 
         $compteur = $repo->countByCandidat($candidat_id);
-
         //dd($compteur);
+
+        
+
+        //$views=$repo3->findViewsRecruteurForCandidat($candidat_id);
+        //dd($view);
+
+        
 
         //$test = $repo2 ->countByCV($candidat_id);
         //dd($test);
 
         return $this->render('candidat/dashboard_candidat.html.twig',[
             'compteur'=>$compteur,
+            //'view'=>$view,
         ]);
     }
 
