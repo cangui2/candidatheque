0b2ffce67b73a338b605410ca73fe9a8bb36e9c8
Julien Canguilieme <canguijc@gmail.com>
2020-12-03 12:03:36 +0100

    finalisation 1 er algo matching en attente competence candidat

--- src/Service/MatchingService.php
+++ src/Service/MatchingService.php
@@ -4,63 +4,55 @@ namespace App\Service;
 
 
 
-use App\Repository\CVRepository;
+
 use App\Repository\OffreRepository;
-use App\Repository\PostuleRepository;
+
 
 class MatchingService {
 
     protected $offreRepo;
-    protected $postuleRepo;
-    protected $cvRepo;
 
-    public function __construct(OffreRepository $offreRepo, PostuleRepository $postuleRepo,CVRepository $cvRepo){
+
+    public function __construct(OffreRepository $offreRepo){
 
         $this->offreRepo=$offreRepo;
-        $this->postuleRepo=$postuleRepo;
-        $this->cvRepo=$cvRepo;
+
+
 
     }
 
     public function matchingOfferVsCvCandidat($idOffer,$skillCvCandidat){
 
 
-        // algorithme de comparaison des competences offre(s) vs candidat(s) //
-        // En attente de  mise en place fonction et des varibales via la tables.
-        // Test
-        /*
+     /* jeux de test local
+
         $idOffer = "offre n°150";
-
         $skillOffer = array( "A1101", "A1102", "A1120","A1115");
-
-
-
         $skillCvCandidat = array (
             "cv1" => array("A1102", "A1101", "A1115"),
             "cv2" => array("A1104", "A1103", "A1101"),
             "cv3" => array("A1102", "A1108", "A1109")
         );
         */
+        // On recupere le tableau des competence par rapport a l'ID
         $skillOffer=$this->offreRepo->findCompetenceByOffer($idOffer);
 
-
-        $test1=[];
+        // On lis le tableau tableau competence offre pour extraire juste les données de competences
+        $offer=[];
         foreach ($skillOffer as $key =>$value){
-            $test1[]=$value['competence_id'];
+            $offer[]=$value['competence_id'];
 
         }
+        // on lit les competences du cv et on le compare avec les competences offres
         $results=[];
         foreach ($skillCvCandidat as $key2 => $value2) {
 
-                    $test2=$value2;
-                    $result[$key2] = array_intersect($test1, $test2);
+                    $candidat=$value2;
+                    $result[$key2] = array_intersect($offer, $candidat);
 
-                    $results[$idOffer][$key2]=(count($result[$key2])*100)/(count($test1));
+                    $results['idOffre'][$idOffer]['idCandidat'][$key2]=(count($result[$key2])*100)/(count($offer));
 
         }
-
-
-
         return $results;
     }
 
