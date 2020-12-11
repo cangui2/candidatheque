3095c527ad1233dde52d1db1ac676b827296a54d
frc <fcaumartin@gmail.com>
2020-12-11 18:42:14 +0100

    test api_cv

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Competence;
 use App\Repository\CompetenceRepository;
+use App\Repository\MetierRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,10 +15,31 @@ class ApiController extends AbstractController
 {
 
     protected $competenceRepo;
+    protected $metierRepo;
 
 
-    public function __construct(CompetenceRepository $competenceRepo){
+    public function __construct(CompetenceRepository $competenceRepo, MetierRepository $metierRepo){
         $this->competenceRepo = $competenceRepo;
+        $this->metierRepo = $metierRepo;
+    }
+
+    /**
+     * @Route("/api/cv/metiers/{libelle}", name="api_cv_metiers")
+     *
+     * use in react-cv
+     */
+    public function searchMetierAndLibelle(string $libelle = "")
+    {
+        $resultat = $this->metierRepo->createQueryBuilder('m')
+            ->select('m.id as id', 'm.libelle as libelle')
+            ->where('m.libelle LIKE :libelle')
+            ->setParameter('libelle', '%'.$libelle.'%')
+            ->setMaxResults(50)
+            ->getQuery()
+            ->getResult();
+
+        // }
+        return new JsonResponse($resultat);
     }
 
     /**
@@ -35,13 +57,10 @@ class ApiController extends AbstractController
             ->getQuery()
             ->getResult();
 
-        // $resultats = [];
-        // foreach ($entities as $e) {
-        //     $resultats[] = $e->getLibelle();
         // }
-
-       
         return new JsonResponse($entities);
-
     }
+
+
+
 }
