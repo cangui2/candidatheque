52ce63e929042d98648158b52114ffed20b77e81
francois <fcaumartin@gmail.com>
2020-12-23 16:48:58 +0100

    wip

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\DepartementRepository;
 use App\Repository\RomeRepository;
+use App\Repository\VilleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -32,6 +34,33 @@ class TestController extends AbstractController
             'rome' => $rome,
            
         ]);
+    }
+
+    /**
+     * @Route("/test2", name="test2")
+     */
+    public function test2(VilleRepository $repo, DepartementRepository $repo2): Response
+    {
+        $ville1 = $repo->find(31722);
+
+        $tab = [];
+
+        $villes = $repo2->find(81)->getVilles();
+        dd($repo->findBy([ "departement" => 32]));
+
+        foreach($repo->findBy([ "departement" => 32]) as $v) {
+
+        }
+
+        $ville2 = $repo->find(31704);
+
+        //$tab[] = $ville1;
+        
+        $distance = sqrt(pow($ville1->getLongitude()-$ville2->getLongitude(), 2) + pow($ville1->getLatitude()-$ville2->getLatitude(), 2)) * 111.16;
+        
+        $tab[] = $distance;
+        
+        return $this->json($tab);
     }
 
 
