898fa1944e5fdbb826b5af8d259443945bc25cb2
francois <fcaumartin@gmail.com>
2020-12-24 10:11:23 +0100

    cv react

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('cv', './assets/cv/index.js')
+    .addEntry('cv', './assets/cv_public/index.js')
     .addEntry('react_recherche', './assets/react_recherche.js')
     .addEntry('test_form', './assets/test_form/index.js')
     //.addEntry('dashboard_offre', './assets/dashboard_offre/graph_camembert.js')
