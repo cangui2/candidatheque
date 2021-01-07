664f92b772784f18ec4c9dba1d0249f9ca9d7f4e
julien canguilieme <canguijc@gmail.com>
2021-01-07 16:14:22 +0100

    ne pas pousser sur test

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -2,12 +2,22 @@
 
 namespace App\Controller;
 
+
+use App\Repository\OffreRepository;
+
+use phpDocumentor\Reflection\Type;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RechercheController extends AbstractController
 {
+
+
+
+
     /**
      * @Route("/recherche", name="recherche")
      */
@@ -23,11 +33,37 @@ class RechercheController extends AbstractController
     /**
      * @Route("/recherche_liste", name="recherche_liste")
      */
-    public function liste(): Response
+    public function liste(OffreRepository $offreRepo, Request $request): Response
     {
+
+        $metier = $request->query->get("metier");
+        $ville = $request->query->get("ville");
+        $secteur = $request->query->get("secteur");
+        $typeContrat=$request->query->get("contrat");
+
+        $query = $offreRepo->createQueryBuilder('o')
+            ->select('o.id', 'o.titre as titre', 'o.datePublication as publication','met.id as idmetier')
+            ->leftJoin('o.metier','met')
+            ->leftJoin('o.ville','vil')
+            ->leftJoin('o.type','typ')
+            ->andWhere('met.id like :metier and vil.id like :ville and typ.id like :contrat ')
+            ->setParameters(array(
+                'metier'=>'%'.$metier.'%',
+                'ville'=>'%'.$ville.'%',
+                'contrat'=>'%'.$typeContrat.'%',
+            ));
+
+        $entities=$query->distinct()->getQuery()
+            ->setMaxResults(30)
+            ->getResult();
+        $test=new JsonResponse($entities);
+
+
+
         return $this->render('recherche/liste.html.twig', [
             'controller_name' => 'RechercheController',
             'variaTest' => rand(0, 9),
+            'test'=>$test,
         ]);
     }
 
