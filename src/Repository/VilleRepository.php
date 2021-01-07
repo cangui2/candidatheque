25cb41cdabb2ef3399b0fd80d9188563ad8f9ec1
julien canguilieme <canguijc@gmail.com>
2021-01-07 16:15:02 +0100

    api searchAround

--- src/Repository/VilleRepository.php
+++ src/Repository/VilleRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Ville;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * @method Ville|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,6 +18,30 @@ class VilleRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Ville::class);
+    }
+
+    public function searchAround($id_ville, $rayon)
+    {
+        $ville = $this->find($id_ville);
+        $latitude=$ville->getLatitude();
+        $longitude=$ville->getLongitude();
+
+        $query = $this->createQueryBuilder('v')
+            ->select('v.id as id','v.nom as nom ','v.latitude as latitude','v.longitude as longitude')
+            ->addSelect('(6371 * acos(cos(radians(:latitude)) * cos(radians(v.latitude)) * cos(radians(v.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(v.latitude)))) as distance')
+            ->having('distance < :rayon')
+            ->setParameters(array(
+                'rayon'=>'%'.$rayon.'%',
+                'latitude'=>'%'.$latitude.'%',
+                'longitude'=>'%'.$longitude.'%',
+            ));
+
+
+        $entities=$query->distinct()->getQuery()
+        ->setMaxResults(30)
+        ->getResult();
+
+        return $entities;
     }
 
     // /**
